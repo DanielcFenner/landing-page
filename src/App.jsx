bfb3c23e353e3efdd12751aff676d2adb0c91d24
Daniel Fenner <dcfenner@gmail.com>
2022-06-09 00:56:07 -0400

    solid progress

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,29 @@
-import logo from './logo.svg';
-import styles from './App.module.css';
+import logo from "./logo.svg";
+import "./App.css";
+import portrait from "./assets/portrait.webp";
 
 function App() {
   return (
-    <div class={styles.App}>
-      <header class={styles.header}>
-        <img src={logo} class={styles.logo} alt="logo" />
-        <p>
-          Edit <code>src/App.jsx</code> and save to reload.
-        </p>
-        <a
-          class={styles.link}
-          href="https://github.com/solidjs/solid"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn Solid
-        </a>
-      </header>
+    <div class="app">
+      <div class="gradient"></div>
+      <main>
+        <div class="left">
+          <img src={portrait}></img>
+          <h1>Daniel Fenner</h1>
+          <p>gamer streamer runner</p>
+          <div class="buttonGrid">
+            <button>Twitch</button>
+            <button>YouTube</button>
+            <button>Twitter</button>
+            <button>Instagram</button>
+            <button>TikTok</button>
+          </div>
+        </div>
+        <div class="right">
+          <h1>Live now</h1>
+          <div class="video"></div>
+        </div>
+      </main>
     </div>
   );
 }
