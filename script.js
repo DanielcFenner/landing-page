c98bb62d8126592440c4cfa10288a74e62559e63
Daniel Fenner <danielfennerdev@gmail.com>
2024-03-03 00:14:55 -0500

    just need to add affiliate links after

--- script.js
+++ script.js
@@ -8,19 +8,20 @@ canvas.height = canvas.clientHeight * dpr;
 ctx.scale(dpr, dpr);
 
 // Star representation
+let starSpeed = 0.1;
 function Star() {
-  this.x = Math.random() * canvas.width;
-  this.y = Math.random() * canvas.width;
+  this.x = Math.random() * (canvas.width / dpr);
+  this.y = Math.random() * (canvas.height / dpr);
   this.size = Math.floor(Math.random() * 3) + 1; // 1, 2, or 3 pixels
   switch (this.size) {
     case 1:
-      this.speed = 0.2; // Fastest speed stars
+      this.speed = starSpeed + 0.1; // Fastest speed stars
       break;
     case 2:
-      this.speed = 0.15; // Medium speed stars
+      this.speed = starSpeed + 0.05; // Medium speed stars
       break;
     case 3:
-      this.speed = 0.1; // Slowest speed stars
+      this.speed = starSpeed; // Slowest speed stars
       break;
     default:
       this.speed = 1; // A default in case something unexpected happens
@@ -28,7 +29,7 @@ function Star() {
 }
 
 let stars = [];
-const numStars = 1000; // Adjust for density
+const numStars = 100; // Adjust for density
 
 // Initialize stars
 for (let i = 0; i < numStars; i++) {
@@ -41,13 +42,13 @@ function animate() {
 
   ctx.fillStyle = "white";
   stars.forEach((star) => {
-    ctx.fillRect(star.x, star.y, star.size, star.size);
+    ctx.fillRect(star.x, star.y, star.size, star.size); // Draw square
     star.y -= star.speed;
 
     // Reset stars that go off-screen
     if (star.y < -10) {
-      star.y = canvas.height + Math.random() * 50;
-      star.x = Math.random() * canvas.width;
+      star.y = canvas.height / dpr + Math.random() * 50;
+      star.x = Math.random() * (canvas.width / dpr);
     }
   });
 
